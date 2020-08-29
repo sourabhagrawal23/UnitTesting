e03a228d4fb95a298161220f8bb17af3e529eba2
sourabhagrawal23 <sourabhkhs23@gmail.com>
2020-08-29 23:55:21 +0530

    fare Utils added

---
+++ public_static/script.js
@@ -0,0 +1,25 @@
+//document.ready function
+$(function () {
+    let kmBox = $('#km')
+    let minBox = $('#min')
+    let calcBtn = $('#calcfare')
+    let fareDiv = $('#fare')
+    let rateBtn = $('#getrates')
+    let rateDiv = $('#rates')
+
+    calcBtn.click(function () {
+        $.post('/calcfare', {
+                km: kmBox.val(),
+                min: minBox.val()
+            },
+            function (data) {
+                fareDiv.text(data.fare)
+            })
+    })
+
+    rateBtn.click(function () {
+        $.gte('/rate', function (data) {
+            rateDiv.test(JSOn.stringify(data))
+        })
+    })
+})
